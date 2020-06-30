c6d63d792c97f12b6585a27a10cebd8241833843
Daniel Cole <danielscole1@gmail.com>
2020-06-30 19:26:14 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,27 +20,27 @@ end
 
 def remove_element_from_end_of_array(array)
   array = ["I", "am", "really", "learning", "arrays!"]
-  remove_element_from_end_of_array = array.pop
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  remove_element_from_start_of_array = array.shift
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  retrieve_element_from_index = array[2]
+  array[2]
 end
 
 def retrieve_first_element_from_array(array)
-   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  retrieve_first_element_from_array = array[0]
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  retrieve_last_element_from_array = array[-1]
+  array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
